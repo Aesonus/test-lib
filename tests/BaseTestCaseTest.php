e2c6400b86c190f59ccd9ffa36aa08e28402dce4
Aesonus <corylcomposinger@gmail.com>
2020-07-20 10:16:28 -0500

    Removed deprecated methods

--- tests/BaseTestCaseTest.php
+++ tests/BaseTestCaseTest.php
@@ -29,50 +29,8 @@ class BaseTestCaseTest extends TestCase
 
     protected function setUp(): void
     {
-        \PHPUnit\Framework\Error\Notice::$enabled = false;
         $this->baseTestCase = $this->getMockForAbstractClass(BaseTestCase::class);
-        $this->mockObject = $this->getMockBuilder(TestHelper::class)->setMethods([static::$methodToInvoke])->disableOriginalConstructor()
-            ->getMock();
-        //$this->mockObject->expects($this->once())->method(static::$methodToInvoke)->willReturn(true);
         parent::setUp();
-    }
-
-    public function parametersDataProvider()
-    {
-        return [
-            [[NULL]],
-            [['arg1', new \stdClass()]],
-            [['arg1', new \stdClass(), 56]],
-            [['arg1', new \stdClass(), 54.673]]
-        ];
-    }
-
-    private function getMockExpectedParams($expected_params)
-    {
-        return array_map(function ($value) {
-            return $this->equalTo($value);
-        }, $expected_params);
-    }
-
-    /**
-     * @test
-     */
-    public function testInvokeConstructorWithNoParametersWorks()
-    {
-        $this->mockObject->expects($this->once())->method(static::$methodToInvoke);
-        $this->baseTestCase->invokeConstructor($this->mockObject);
-    }
-
-    /**
-     * @test
-     * @dataProvider parametersDataProvider
-     */
-    public function invokeConstructorWithParametersCallsTheRightMethod($expected_params)
-    {
-        $mock_expected_params = $this->getMockExpectedParams($expected_params);
-        //Set expectation
-        call_user_func_array([$this->mockObject->expects($this->once())->method(static::$methodToInvoke), 'with'], $mock_expected_params);
-        $this->baseTestCase->invokeConstructor($this->mockObject, $expected_params);
     }
 
     /**
